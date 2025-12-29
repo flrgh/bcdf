abad51452b4ef8c9450867b7fb21d593c45dab6c
Michael Martin <flrgh@protonmail.com>
2025-12-29 14:41:31 -0800

    chore(html): use LazyLock for CSS selectors

--- src/bandcamp.rs
+++ src/bandcamp.rs
@@ -2,17 +2,66 @@ use crate::types::{DateTime, Duration, Track};
 use anyhow::Context;
 use scraper::{Html, Selector};
 use serde_json as json;
+use std::sync::LazyLock;
 
 pub(crate) const FEED_URL: &str = "https://daily.bandcamp.com/feed/";
 
-fn css_selector(s: &str) -> anyhow::Result<Selector> {
-    Ok(match Selector::parse(s) {
-        Ok(selector) => selector,
-        Err(e) => {
-            anyhow::bail!("generating a CSS selector '{s}': {e}");
-        }
-    })
+struct HtmlSelector {
+    text: String,
+    selector: Selector,
 }
+
+impl HtmlSelector {
+    fn try_new(s: &str) -> anyhow::Result<Self> {
+        match Selector::parse(s) {
+            Ok(sel) => Ok(Self {
+                text: s.to_string(),
+                selector: sel,
+            }),
+            Err(e) => {
+                anyhow::bail!("failed parsing CSS selector from '{s}': {e}");
+            }
+        }
+    }
+
+    fn try_new_meta(name: &str) -> anyhow::Result<Self> {
+        let s = format!(r#"meta[property="{name}"]"#);
+        Self::try_new(s.as_str())
+    }
+
+    fn select<'a, 'b>(&'a self, doc: &'b Html) -> scraper::html::Select<'b, 'a> {
+        doc.select(&self.selector)
+    }
+}
+
+macro_rules! selector {
+    ($name:ident, $s:expr) => {
+        selector!($name, $s, HtmlSelector::try_new);
+    };
+    ($name:ident, $s:expr, $fn:path) => {
+        static $name: LazyLock<HtmlSelector> =
+            LazyLock::new(|| $fn($s).expect("invalid CSS selector"));
+    };
+}
+
+selector!(DAILY_ARTICLE, "#p-daily-article");
+selector!(META_TITLE, "og:title", HtmlSelector::try_new_meta);
+selector!(META_URL, "og:url", HtmlSelector::try_new_meta);
+selector!(
+    META_PUBLISHED,
+    "article:published_time",
+    HtmlSelector::try_new_meta
+);
+selector!(
+    META_MODIFIED,
+    "article:modified_time",
+    HtmlSelector::try_new_meta
+);
+selector!(
+    META_DESCRIPTION,
+    "og:description",
+    HtmlSelector::try_new_meta
+);
 
 #[derive(Debug, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
 pub(crate) struct TrackInfo {
