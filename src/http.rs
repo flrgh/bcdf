b98887650c941f23683c1e34f151af9563be01f2
Michael Martin <flrgh@protonmail.com>
2026-08-04 12:11:16 -0700

    fix(http): set a proper User-Agent header

--- src/http.rs
+++ src/http.rs
@@ -1,7 +1,11 @@
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue, CACHE_CONTROL, PRAGMA, REFERER};
 use reqwest::Client;
 
-const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0";
+const DEFAULT_USER_AGENT: &str = concat!(
+    "Bandcamp Daily Blog Fetcher/",
+    env!("CARGO_PKG_VERSION"),
+    " (https://github.com/flrgh/bcdf)"
+);
 const DEFAULT_HEADERS: &[(HeaderName, &str)] = &[
     (PRAGMA, "no-cache"),
     (CACHE_CONTROL, "no-cache"),
@@ -15,8 +19,12 @@ pub(crate) fn client() -> reqwest::Client {
             .map(|(name, value)| (name.clone(), HeaderValue::from_static(value))),
     );
 
+    let user_agent = std::env::var("BCDF_USER_AGENT")
+        .ok()
+        .unwrap_or_else(|| DEFAULT_USER_AGENT.to_string());
+
     Client::builder()
-        .user_agent(USER_AGENT)
+        .user_agent(user_agent)
         .default_headers(headers)
         .build()
         .expect("unreachable!")
